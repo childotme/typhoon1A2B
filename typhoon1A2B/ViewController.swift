67a0963d2e4b4a57455795d482b93512ff088cfa
niki <nikitinwei@gmail.com>
2024-10-02 23:13:21 +0800

    Initial Commit

---
+++ typhoon1A2B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  typhoon1A2B
+//
+//  Created by 俞廷瑋 on 2024/10/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ typhoon1A2BTests/typhoon1A2BTests.swift
@@ -0,0 +1,17 @@
+//
+//  typhoon1A2BTests.swift
+//  typhoon1A2BTests
+//
+//  Created by 俞廷瑋 on 2024/10/2.
+//
+
+import Testing
+@testable import typhoon1A2B
+
+struct typhoon1A2BTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
